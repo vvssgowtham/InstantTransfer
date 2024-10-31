21c5dd2b8fb13907899632cb15bfca3981a0409d
vvssgowtham <gowthamvvss@gmail.com>
2024-10-31 21:58:06 +0530

    Remove redirects for authenticated users on signup and signin pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate,
 } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -14,20 +13,12 @@ const Dashboard = lazy(() => import("./pages/Dashboard"));
 const LoadingOverlay = lazy(() => import("./components/Loading"));
 
 function App() {
-  const token = localStorage.getItem("token");
-
   return (
     <Router>
       <Suspense fallback={<LoadingOverlay />}>
         <Routes>
-          <Route
-            path="/signup"
-            element={token ? <Navigate to="/" /> : <Signup />}
-          />
-          <Route
-            path="/signin"
-            element={token ? <Navigate to="/" /> : <Login />}
-          />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Login />} />
           <Route
             path="/"
             element={<ProtectedRoute element={<Dashboard />} />}
